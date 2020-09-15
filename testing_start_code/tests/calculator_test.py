c8ff42f75734ed3ef940cd56017af7c2d76d30c9
svvishjs <andreww92@live.com>
2020-09-15 20:58:56 +0100

    added test functions for subtract, divide and multiply. All tests pass

--- testing_start_code/tests/calculator_test.py
+++ testing_start_code/tests/calculator_test.py
@@ -3,5 +3,14 @@ from src.calculator import add, subtract, multiply, divide
 
 class TestCalculator(unittest.TestCase):
     
-    def _test_add(self):
+    def test_add(self):
         self.assertEqual(5, add(2, 3))
+
+    def test_subtract(self):
+        self.assertEqual(3, subtract(10, 7))
+
+    def test_divide(self):
+        self.assertEqual(2, divide(10, 5))
+
+    def test_multiply(self):
+        self.assertEqual(1000, multiply(100, 10))
